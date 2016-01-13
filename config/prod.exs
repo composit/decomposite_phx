68cc313508970bcca28d66e08a2d7a6ca3745dc9
Mr. Matt <matt@composition9.com>
2016-01-13 11:57:55 -0700

    correct url in prod config

--- config/prod.exs
+++ config/prod.exs
@@ -13,7 +13,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :decomposite, Decomposite.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "example.com"],
+  url: [host: "decomposite.com"],
   cache_static_manifest: "priv/static/manifest.json"
 
 # Do not print debug messages in production
